131fce5e15d770a4f01cbbae0b33a4bd70e95d42
nadire22 <112009353+nadire22@users.noreply.github.com>
2022-09-11 14:25:45 -0400

    Update Triangle.kt

--- Triangle.kt
+++ Triangle.kt
@@ -3,8 +3,7 @@ abstract class Triangle (_name:String): Shape(_name) {
     val base = 10;
 
     var TriangleArea = 0.0;
-
-    //test
+    
     open override fun getArea(): Double {
         TriangleArea = (height * base) / 2 ;
         return getArea()
